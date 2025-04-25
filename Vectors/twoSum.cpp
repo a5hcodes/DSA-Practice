71d01906253bd16b6c5d35b7c6ac084cb998f625
a5hcodes <rajana22cs@student.mes.ac.in>
2025-04-25 08:45:34 +0530

    D-2 25/4/25

--- Vectors/twoSum.cpp
+++ Vectors/twoSum.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
 using namespace std;
 /*
 Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
@@ -38,11 +39,62 @@ vector<int> twoSum(vector<int> &nums, int target)
     return {}; // in case no solution is found (just to satisfy return for all code paths)
 }
 
+// Two Pointers Approach
+// Time Complexity : O(n log n)
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    vector<pair<int, int>> numIndex;
+    for (int i = 0; i < n; i++)
+    {
+        numIndex.push_back({nums[i], i});
+    }
 
+    sort(numIndex.begin(), numIndex.end());
 
+    int i = 0, j = n - 1;
 
+    while (i < j)
+    {
+        int tarSum = numIndex[i].first + numIndex[j].first;
 
+        if (tarSum == target)
+        {
+            return {numIndex[i].second, numIndex[j].second};
+        }
+        else if (tarSum < target)
+        {
+            i++;
+        }
+        else
+        {
+            j--;
+        }
+    }
+    return {};
+}
 
+// Hashmaps Approach for most optimal solution
+// Time Complexity : O(n)
+
+vector<int> twoSum(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    unordered_map<int, int> numMap; // this is empty intialilly 
+
+    for (int i = 0; i < n; i++)
+    {
+        int complement = target - nums[i]; // the difference gets stored
+
+        if (numMap.find(complement) != numMap.end()) // checks if the diffreence is in the hashmap or not
+        {
+            return {numMap[complement], i}; // returns the complement and current position 
+        }
+
+        numMap[nums[i]] = i; // if not found , the current position value gets stored in the hashmap
+    }
+    return {};
+}
 
 int main()
 {
