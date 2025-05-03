447cbe70c9005d6aa5e16a06c465f8448abc7822
a5hcodes <rajana22cs@student.mes.ac.in>
2025-05-03 15:40:03 +0530

    D-9 3/5/25

---
+++ Vectors/missingNumber.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+// int missingNumber(vector<int> &nums)
+// {
+//     int n = nums.size();
+
+//     sort(nums.begin(), nums.end());
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         if (nums[i] != i)
+//         {
+//             return i;
+//         }
+//     }
+//     return n;
+// }
+
+int missingNumber(vector<int> &nums)
+{
+    int n = nums.size();
+
+    unordered_set<int> seen;
+
+    for (int i = 0; i < n; i++)
+    {
+        seen.insert(nums[i]);
+    }
+    for (int i = 0; i <= n; i++)
+    {
+        if (seen.find(i) == seen.end())
+        {
+            return i;
+        }
+    }
+    return n;
+}
+
+int main()
+{
+    vector<int> vec = {3, 0, 1, 2};
+
+    cout << "Missing Number: " << missingNumber(vec) << endl;
+
+    return 0;
+}
