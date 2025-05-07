3eb82ba9d83a1ef45cede87dde3531d94ee98b6e
a5hcodes <rajana22cs@student.mes.ac.in>
2025-05-07 13:01:18 +0530

    D-13 7/5/25

--- Vectors/keyboardRow.cpp
+++ Vectors/keyboardRow.cpp
@@ -4,8 +4,52 @@
 #include <unordered_set>
 using namespace std;
 
+/*
+500. Keyboard Row
+Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of American keyboard like the image below.
+
+Note that the strings are case-insensitive, both lowercased and uppercased of the same letter are treated as if they are at the same row.
+
+In the American keyboard:
+
+the first row consists of the characters "qwertyuiop",
+the second row consists of the characters "asdfghjkl", and
+the third row consists of the characters "zxcvbnm".
+
+
+
+Example 1:
+
+Input: words = ["Hello","Alaska","Dad","Peace"]
+
+Output: ["Alaska","Dad"]
+
+Explanation:
+
+Both "a" and "A" are in the 2nd row of the American keyboard due to case insensitivity.
+
+Example 2:
+
+Input: words = ["omk"]
+
+Output: []
+
+Example 3:
+
+Input: words = ["adsdf","sfd"]
+
+Output: ["adsdf","sfd"]
+*/
+
+// given: a string array with some words.
+// to find: the words that are letters from the same row of the american keyword qwerty
+
+// Optimal Solution
+// Time Complexity : O(n)
+
 vector<string> findWords(vector<string> &words)
 {
+    // we use hashset to match the letters qucikly
     unordered_set<char> row1 = {'q', 'w', 'e', 'r', 't',
                                 'y', 'u', 'i', 'o', 'p'};
 
@@ -15,45 +59,46 @@ vector<string> findWords(vector<string> &words)
     unordered_set<char> row3 = {'z', 'x', 'c', 'v',
                                 'b', 'n', 'm'};
 
-    vector<string> result;
+    vector<string> result; // to store the output
 
+    // Start processing each word
     for (string word : words)
     {
-        string lower = word;
+        string lower = word; // we copy each word to the var lower // first word: Hello
 
         for (char &c : lower)
         {
-            c = tolower(c);
+            c = tolower(c); // we convert each word to lowercase from the array in lower // hello
         }
 
-        unordered_set<char> *currRow;
+        unordered_set<char> *currRow; // we create an empty pointer that points to the rows where the ltters are present
 
-        if (row1.count(lower[0]))
+        if (row1.count(lower[0])) // row1 has the first letter of the string in lower
         {
-            currRow = &row1;
+            currRow = &row1; // we store the address of row1 to currRow pointer // h not present
         }
         else if (row2.count(lower[0]))
         {
-            currRow = &row2;
+            currRow = &row2; // h is present here for hello
         }
         else
         {
             currRow = &row3;
         }
 
-        bool is_valid = true;
+        bool is_valid = true; // we set the bool var to true initially
 
-        for (char c : lower)
+        for (char c : lower) // we check other letters
         {
-            if (currRow->count(c) == 0)
+            if (currRow->count(c) == 0) // we check if other letters from the word is 0 i.e if there are no valid letters
             {
-                is_valid = false;
-                break;
+                is_valid = false; // we return false
+                break;            // end the loop
             }
         }
-        if (is_valid)
+        if (is_valid) // all the letters are from a single row
         {
-            result.push_back(word);
+            result.push_back(word); // we push the word to the vector result and return it!
         }
     }
     return result;
