b3849a5da5f3e3e3f44f944142355045c7511f16
a5hcodes <rajana22cs@student.mes.ac.in>
2025-05-08 13:23:33 +0530

    D-14 8/5/25

--- Vectors/2006.cpp
+++ Vectors/2006.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <cmath>
-#include <unordered_set>
+#include <unordered_map>
 using namespace std;
 
 /*
@@ -52,28 +52,77 @@ Constraints:
 // Brute Force Approach
 //  Time Complexity : O(n2)
 
+// int countKDifference(vector<int> &nums, int k)
+// {
+//     int n = nums.size();
+//     int count = 0;
+
+//     for (int i = 0; i < n; i++)
+//     {
+//         for (int j = i + 1; j < n; j++)
+//         {
+//             int diff = abs(nums[i] - nums[j]);
+//             if (diff == k)
+//             {
+//                 count++;
+//             }
+//         }
+//     }
+//     return count;
+// }
+
+// Optimal Solution using Hashmaps
+//  Time complexity : O(n)
+
 int countKDifference(vector<int> &nums, int k)
 {
     int n = nums.size();
-    int count = 0;
+    unordered_map<int, int> numCount; // to keep trach of the count of numbers that has diff of k
+    int pairCount = 0; // stores total number of valid pairs 
 
     for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1; j < n; j++)
-        {
-            int diff = abs(nums[i] - nums[j]);
-            if (diff == k)
-            {
-                count++;
-            }
-        }
+        pairCount += numCount[nums[i] + k]; //Check how many times (num + k) has appeared before
+        pairCount += numCount[nums[i] - k]; //Check how many times (num - k) has appeared before
+
+        numCount[nums[i]]++; //Now update the count of this number for future comparisons
+
+       /*
+        Dry Run Example: nums = [1, 2, 2, 1], k = 1
+
+        Step 1: num = 1
+        - num + k = 2 → not in numCount
+        - num - k = 0 → not in numCount
+        → pairCount = 0
+        → numCount = {1: 1}
+
+        Step 2: num = 2
+        - num + k = 3 → not in numCount
+        - num - k = 1 → found 1 → pairCount += 1
+        → pairCount = 1
+        → numCount = {1: 1, 2: 1}
+
+        Step 3: num = 2
+        - num + k = 3 → not in numCount
+        - num - k = 1 → found 1 → pairCount += 1
+        → pairCount = 2
+        → numCount = {1: 1, 2: 2}
+
+        Step 4: num = 1
+        - num + k = 2 → found 2 twice → pairCount += 2
+        - num - k = 0 → not in numCount
+        → pairCount = 4
+        → numCount = {1: 2, 2: 2}
+        */
     }
-    return count;
+
+    return pairCount;
 }
 
-int main() {
-    vector<int> vec =  {1,2,2,1};
+int main()
+{
+    vector<int> vec = {1, 2, 2, 1};
     int k = 1;
 
-    cout<< "Number of difference of k in array: " << countKDifference(vec, k) << endl;
+    cout << "Number of difference of k in array: " << countKDifference(vec, k) << endl;
 }
