3b206b6620394c5dc2639f34e2ef4d2c7109056f
a5hcodes <rajana22cs@student.mes.ac.in>
2025-05-11 13:26:31 +0530

    D-17 11/5/25

--- Vectors/intersection.cpp
+++ Vectors/intersection.cpp
@@ -30,28 +30,28 @@ Constraints:
 // Brute Force Approach
 //  Time Complexity : O(n1*n2)
 
-// vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
-// {
-//     int n1 = nums1.size();
-//     int n2 = nums2.size();
+vector<int> intersection(vector<int> &nums1, vector<int> &nums2)
+{
+    int n1 = nums1.size();
+    int n2 = nums2.size();
 
-//     vector<int> res;
-//     unordered_set<int> added;
+    vector<int> res;
+    unordered_set<int> added;
 
-//     for (int i = 0; i < n1; i++)
-//     {
-//         for (int j = 0; j < n2; j++)
-//         {
-//             if (nums1[i] == nums2[j] && added.find(nums1[i]) == added.end())
-//             {
-//                 res.push_back(nums1[i]);
-//                 added.insert(nums1[i]);
-//                 break;
-//             }
-//         }
-//     }
-//     return res;
-// }
+    for (int i = 0; i < n1; i++)
+    {
+        for (int j = 0; j < n2; j++)
+        {
+            if (nums1[i] == nums2[j] && added.find(nums1[i]) == added.end())
+            {
+                res.push_back(nums1[i]);
+                added.insert(nums1[i]);
+                break;
+            }
+        }
+    }
+    return res;
+}
 
 //Optimal Approach
 //Time Complexity: O(n)
